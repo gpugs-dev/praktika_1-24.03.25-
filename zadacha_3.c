a1c3925bd89260d69fad5a0de4e89fdd7ec7292d
gpugs-dev <lexaost@bk.ru>
2025-03-24 10:30:04 +0300

    Create zadacha_3.c

---
+++ zadacha_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Введите три числа: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    int sign = (a > 0) - (a < 0);
+    sign *= (b > 0) - (b < 0);
+    sign *= (c > 0) - (c < 0);
+
+    printf("%d\n", sign);
+
+    return 0;
+}
