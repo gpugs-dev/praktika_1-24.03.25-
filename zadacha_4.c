ba153a9fc809f98f525118ea6ccc3dae8e904c63
gpugs-dev <lexaost@bk.ru>
2025-03-24 10:35:10 +0300

    Update zadacha_4.c

--- zadacha_4.c
+++ zadacha_4.c
@@ -6,9 +6,9 @@ int main() {
     printf("Введите целое число: ");
     scanf("%d", &n);
 
-    int isPowerOfTwo = (n > 0) && ((n & (n - 1)) == 0;
+    int dvoika = (n > 0) && ((n & (n - 1)) == 0;
 
-    printf("%d\n", isPowerOfTwo);
+    printf("%d\n", dvoika);
 
     return 0;
 }
