08d848534e5ea122bae8aa6c8708f2958423f9d8
gpugs-dev <lexaost@bk.ru>
2025-03-24 10:50:21 +0300

    Update zadacha_4.c

--- zadacha_4.c
+++ zadacha_4.c
@@ -6,9 +6,9 @@ int main() {
     printf("Введите целое число: ");
     scanf("%d", &n);
 
-    int dvoika = (n > 0) && ((n & (n - 1)) == 0;
+    int dvvoika = (n > 0) && ((n & (n - 1)) == 0);
 
-    printf("%d\n", dvoika);
+    printf("%d\n", dvvoika);
 
     return 0;
 }
